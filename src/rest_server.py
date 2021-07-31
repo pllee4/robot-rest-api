6742c269b0a931c40058f31c9fdb33e674a7b898
pllee4 <pinloon_0428@hotmail.com>
2021-07-31 20:24:36 +0800

    src: rest_server: code refractor
    remove most of the global variable
    
    remove unused dependency
    
    code formatting

--- src/rest_server.py
+++ src/rest_server.py
@@ -1,67 +1,68 @@
 #!/usr/bin/env python
+"""
+rest_server.py
+Created on: Jul 31, 2021 18:03
+Description: REST API Server
+Copyright (c) 2021 Pin Loon Lee (pllee4)
+"""
 # ROS
+import argparse
 import rospy
-import threading
 from actionlib_msgs.msg import GoalStatusArray
 
 # Flask
 from flask import Flask
 from flask_restful import Resource, Api
 
-# Python
-import argparse
-
-# ROS
-status = 0
-text = ""
-new_data_arrived = False
+robot_data = {}
+robot_data["data_arrived"] = False
 
 
 def move_base_status_callback(data):
-    # TODO: update data and pass to server
-    if (data.status_list):
-        global status
-        global text
-        global new_data_arrived
-        status = data.status_list[0].status
-        text = data.status_list[0].text
-        new_data_arrived = True
+    """callback from ROS topic /move_base/status"""
+    if data.status_list:
+        robot_data["status"] = data.status_list[0].status
+        robot_data["text"] = data.status_list[0].text
+        robot_data["data_arrived"] = True
 
 
-def ros_node():
-    rospy.init_node('ros_rest_server', disable_signals=True)
-    rospy.Subscriber("/move_base/status", GoalStatusArray,
-                     move_base_status_callback)
+class MoveBaseStatus(Resource):
+    """queried by /api/robot/status"""
+    @staticmethod
+    def get():
+        """client request or curl"""
+        if robot_data["data_arrived"]:
+            robot_data["data_arrived"] = False
+            response = {
+                "status": robot_data["status"],
+                "text": robot_data["text"],
+            }, 200
+        else:
+            response = {"message": "Invalid status value"}, 400
+        return response
 
 
-threading.Thread(target=ros_node).start()
+# ROS
+rospy.init_node("ros_rest_server", disable_signals=True)
+rospy.Subscriber(
+    "/move_base/status", GoalStatusArray, move_base_status_callback
+)
 
 # Flask
 app = Flask("REST Server")
 api = Api(app)
-
-
-class MoveBaseStatus(Resource):
-    def get(self):
-        # TODO: get data from robot_node
-        # TODO: check whether there is topic
-        global new_data_arrived
-        if (new_data_arrived):
-            new_data_arrived = False
-            return {'status': status, 'text': text}, 200
-        else:
-            return {'message': "Invalid status value"}, 400
-
-
 api.add_resource(MoveBaseStatus, "/api/robot/status")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="REST Server")
     parser.add_argument(
         "port_number", nargs="?", default="7201", type=int, help="port number"
     )
     parser.add_argument(
-        "ip_address", nargs="?", default="127.0.0.1", help="ip_address for server"
+        "ip_address",
+        nargs="?",
+        default="127.0.0.1",
+        help="ip_address for server",
     )
     margs = parser.parse_args(rospy.myargv()[1:])
     port_number = margs.port_number
